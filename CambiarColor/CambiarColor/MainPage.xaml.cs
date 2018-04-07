1869f0bd0af305ba9b7fafaa2940b921774eeae8
lorenaquint <lorenaquint@gmail.com>
2018-04-07 14:28:33 -0500

    Ejercicio

--- CambiarColor/CambiarColor/MainPage.xaml.cs
+++ CambiarColor/CambiarColor/MainPage.xaml.cs
@@ -13,5 +13,24 @@ namespace CambiarColor
 		{
 			InitializeComponent();
 		}
-	}
+        private void sld_ValueChanged(object sender, EventArgs e)
+        {
+            var rojo = sldRojo.Value;
+            var verde = sldVerde.Value;
+            var azul = sldAzul.Value;
+            var opacidad = sldOpacidad.Value;
+            Color bgColor = new Color(rojo, verde, azul, opacidad);
+            boxColor.BackgroundColor = bgColor;
+            lbDisplay.Text = ColorHexa(bgColor);
+        }
+
+        private string ColorHexa(Color bgColor)
+        {
+            int rojo = (int)(bgColor.R * 255);
+            int verde = (int)(bgColor.G * 255);
+            int azul = (int)(bgColor.B * 255);
+            int opacidad = (int)(bgColor.A * 255);
+            return $"#{rojo:X2}{verde:X2}{azul:X2}{opacidad:X2}";
+        }
+    }
 }
